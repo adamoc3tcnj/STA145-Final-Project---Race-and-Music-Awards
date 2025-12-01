86f63e580fd691ddabcc2797b7aa2f16a9f676f4
adamoc3tcnj <adamoc3@tcnj.edu>
2025-12-01 15:09:10 -0500

    Add files via upload

---
+++ adamoc3-final_project.R
@@ -0,0 +1,10 @@
+### Load packages 
+# NOTE: you may need to install!
+library(readr)
+
+
+#set working directory 
+setwd("/courses/STA145/adamoc3")
+
+# Load the data
+data <- read_csv("data.csv.csv")
