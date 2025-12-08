55109a91878b8113697bdd360cbed453e18e8322
adamoc3tcnj <adamoc3@tcnj.edu>
2025-12-08 15:47:12 -0500

    Update adamoc3-final_project-2.R

--- adamoc3-final_project-2.R
+++ adamoc3-final_project-2.R
@@ -12,9 +12,6 @@ data <- read_csv("data.csv.csv")
 data_complete <- data %>%
   filter(complete.cases(.))
 
-table(data$race_of_lead_artist, data$awards_won)
-
-
 # Create the contingency table
 contingency_table <- table(data$race_of_lead_artist, data$awards_won)
 print(contingency_table)
